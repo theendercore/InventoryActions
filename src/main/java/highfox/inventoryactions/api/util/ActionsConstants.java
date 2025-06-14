9d29ca7a1a721d6131bef07b08ada70036994f95
TheEnderCore <theendercore@gmail.com>
2025-06-14 20:53:51 +0300

    added id function

--- src/main/java/highfox/inventoryactions/api/util/ActionsConstants.java
+++ src/main/java/highfox/inventoryactions/api/util/ActionsConstants.java
@@ -1,9 +1,13 @@
 package highfox.inventoryactions.api.util;
 
+import net.minecraft.resources.ResourceLocation;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class ActionsConstants {
 	public static final String MODID = "inventoryactions";
 	public static final Logger LOG = LogManager.getLogger(MODID);
+	public static ResourceLocation id(String path) {
+		return new ResourceLocation(MODID,path);
+	}
 }
